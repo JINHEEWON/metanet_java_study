0e5463e9639211c7793be80d9049586b12422c0d
JINHEEWON <diebfusod1228@naver.com>
2023-11-08 19:16:18 +0900

    11/8 commit2

---
+++ JAVA_Basic_08_IO/src/Ex11_Example.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+import kr.or.kosa.UsedMarket;
+
+public class Ex11_Example {
+
+	public static void main(String[] args) {
+		
+		UsedMarket usedMarket = new UsedMarket();
+		usedMarket.run();
+
+	}
+
+}
